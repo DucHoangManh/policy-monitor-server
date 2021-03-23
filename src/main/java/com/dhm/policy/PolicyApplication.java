21fa757196cb3c40931e98a1ea7918af709ed03d
duchm <gelous3@gmail.com>
2021-03-23 17:01:39 +0700

    add mongo bootstrap

--- src/main/java/com/dhm/policy/PolicyApplication.java
+++ src/main/java/com/dhm/policy/PolicyApplication.java
@@ -4,6 +4,12 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.mongodb.MongoDatabaseFactory;
+import org.springframework.data.mongodb.core.convert.DbRefResolver;
+import org.springframework.data.mongodb.core.convert.DefaultDbRefResolver;
+import org.springframework.data.mongodb.core.convert.MappingMongoConverter;
+import org.springframework.data.mongodb.core.mapping.MongoMappingContext;
 
 @SpringBootApplication
 public class PolicyApplication {
--- src/main/java/com/dhm/policy/bootstrap/SpringMongoBootstrap.java
+++ src/main/java/com/dhm/policy/bootstrap/SpringMongoBootstrap.java
@@ -1,16 +1,19 @@
 package com.dhm.policy.bootstrap;
 
+import com.dhm.policy.domain.NetworkPolicy;
+import com.dhm.policy.k8s.K8sClientUser;
 import com.dhm.policy.repository.NetworkPolicyRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.stereotype.Component;
 
-
-public class SpringMongoBootstrap implements ApplicationListener<ContextRefreshedEvent> {
+@Component
+public class SpringMongoBootstrap implements ApplicationListener<ContextRefreshedEvent>, K8sClientUser {
     private NetworkPolicyRepository networkPolicyRepository;
-    private Logger logger = LoggerFactory.getLogger(SpringMongoBootstrap.class);
+    private final Logger logger = LoggerFactory.getLogger(SpringMongoBootstrap.class);
 
     @Autowired
     public void setNetworkPolicyRepository(NetworkPolicyRepository networkPolicyRepository){
@@ -23,7 +26,19 @@ public class SpringMongoBootstrap implements ApplicationListener<ContextRefreshe
     }
 
     public void fetchNetworkPolicy(){
-
+        logger.info("fetch network policies in cluster");
+        client
+                .network()
+                .networkPolicies()
+                .inNamespace("default")
+                .list()
+                .getItems()
+                .forEach(
+                        networkPolicy -> {
+                            NetworkPolicy np = new NetworkPolicy("test",networkPolicy);
+                            networkPolicyRepository.save(np);
+                        }
+                );
     }
 
 }

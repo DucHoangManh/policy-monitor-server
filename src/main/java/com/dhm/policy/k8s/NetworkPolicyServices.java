75fd3c0314e62b54b1035e48aa7385a1053b1d2c
duchm <gelous3@gmail.com>
2021-03-25 19:48:10 +0700

    small refactor

--- src/main/java/com/dhm/policy/k8s/NetworkPolicyServices.java
+++ src/main/java/com/dhm/policy/k8s/NetworkPolicyServices.java
@@ -2,6 +2,7 @@ package com.dhm.policy.k8s;
 
 import com.dhm.policy.domain.VersionedNetworkPolicy;
 import com.dhm.policy.repository.NetworkPolicyRepository;
+import com.dhm.policy.repository.VersionRepository;
 import com.dhm.policy.repository.VersionedNetworkPolicyDAL;
 import com.dhm.policy.repository.VersionedNetworkPolicyDALImpl;
 import io.fabric8.kubernetes.api.model.networking.v1.NetworkPolicy;
@@ -9,10 +10,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 @Service
 public class NetworkPolicyServices implements K8sClientUser {
-    VersionedNetworkPolicyDALImpl versionedNetworkPolicyRepository;
+    private VersionedNetworkPolicyDALImpl versionedNetworkPolicyRepository;
+    private NetworkPolicyRepository networkPolicyRepository;
     public List<NetworkPolicy> getLatestPolicies(){
         return versionedNetworkPolicyRepository
                 .getLatestNetworkPolicy()
@@ -20,8 +23,21 @@ public class NetworkPolicyServices implements K8sClientUser {
                 .map(VersionedNetworkPolicy::getNetworkPolicy)
                 .collect(Collectors.toList());
     }
+    public List<VersionedNetworkPolicy> getLatestVersionedPolicy(){
+        return versionedNetworkPolicyRepository
+                .getLatestNetworkPolicy();
+    }
+    public List<VersionedNetworkPolicy> getAll(){
+        return networkPolicyRepository
+                .findAll();
+    }
+    public Optional<VersionedNetworkPolicy> findById(String id){
+        return networkPolicyRepository
+                .findById(id);
+    }
     @Autowired
-    public void setVersionedNetworkPolicyRepository(VersionedNetworkPolicyDALImpl versionedNetworkPolicyDALImpl){
+    public void NetworkPolicyServices(VersionedNetworkPolicyDALImpl versionedNetworkPolicyDALImpl, NetworkPolicyRepository networkPolicyRepository){
         this.versionedNetworkPolicyRepository = versionedNetworkPolicyDALImpl;
+        this.networkPolicyRepository=networkPolicyRepository;
     }
 }
--- src/main/java/com/dhm/policy/web/NetworkPolicyController.java
+++ src/main/java/com/dhm/policy/web/NetworkPolicyController.java
@@ -1,6 +1,7 @@
 package com.dhm.policy.web;
 
 import com.dhm.policy.domain.VersionedNetworkPolicy;
+import com.dhm.policy.k8s.NetworkPolicyServices;
 import com.dhm.policy.repository.NetworkPolicyRepository;
 import com.dhm.policy.repository.VersionedNetworkPolicyDALImpl;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,27 +12,26 @@ import java.util.Optional;
 
 @RestController
 public class NetworkPolicyController {
-    private final NetworkPolicyRepository networkPolicyRepository;
-    private final VersionedNetworkPolicyDALImpl versionedNetworkPolicyDALImpl;
+    private final NetworkPolicyServices networkPolicyServices;
     @RequestMapping("/policy")
     public List<VersionedNetworkPolicy> listAllVersionedPolicy(@RequestParam(value = "latest",required = false) boolean latest){
         if (!latest) {
-            return networkPolicyRepository.findAll();
+            return networkPolicyServices.getAll();
         }else{
-            return versionedNetworkPolicyDALImpl.getLatestNetworkPolicy();
+            return networkPolicyServices.getLatestVersionedPolicy();
         }
     }
 
     @RequestMapping(value = "/policy/{policyId}", method = RequestMethod.GET)
     public VersionedNetworkPolicy findVersionedNetworkPolicyById(@PathVariable String policyId){
-        Optional<VersionedNetworkPolicy> result = networkPolicyRepository.findById(policyId);
-        return result.orElse(null);
+        return networkPolicyServices
+                .findById(policyId)
+                .orElse(null);
     }
 
 
     @Autowired
-    public NetworkPolicyController(NetworkPolicyRepository networkPolicyRepository, VersionedNetworkPolicyDALImpl versionedNetworkPolicyDALImpl){
-        this.networkPolicyRepository = networkPolicyRepository;
-        this.versionedNetworkPolicyDALImpl = versionedNetworkPolicyDALImpl;
+    public NetworkPolicyController(NetworkPolicyServices networkPolicyServices){
+        this.networkPolicyServices=networkPolicyServices;
     }
 }
